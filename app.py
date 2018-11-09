1e1558078823688545b4e077e81e5b966bded476
bjobek <bjornivar95@gmail.com>
2018-11-09 07:31:52 +0100

    1
    first comming

---
+++ app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, render_template
+
+app = FLask(__name__)
+
+@app.route('/')
+def index():
+    my_list = ["apples","oranges","grapes","pineapples"]
+    return sender_template ("index.html",my_list = my_list)
+
+@app.route("/meow")
+def meow():
+    return "MEOW."
+
+if __name__ == "__main__":
+    app.run(debug=True,host = "0.0.0.0")
